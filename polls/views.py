860f357860679b34ee21bba3c9143aee986d94a8
backups <backupsarea@gmail.com>
2022-06-01 20:34:12 +0300

    generic views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render,get_object_or_404
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
 # Create your views here.
-def home(request):
-    return HttpResponse("datas")
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.all()[:5]
+#detail view
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+#results view
+class ResultsView(generic.DetailView):
+   model = Question
+   template_name = 'polls/results.html'
+#vote view
+def vote(request, question_id):
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist) as e:
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message': "You didnt select a choice",
+        })
+    else:
+        selected_choice.votes+=1
+        selected_choice.save();
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
