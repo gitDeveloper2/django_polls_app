f14993bfd4011efd22aaf13869f2708146bb6f4e
backups <backupsarea@gmail.com>
2022-06-03 16:29:17 +0300

    static files

--- polls/models.py
+++ polls/models.py
@@ -9,6 +9,10 @@ class Question(models.Model):
     pub_date=models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        now=timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
@@ -16,5 +20,3 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from time import timezone
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
@@ -5,17 +7,22 @@ from django.shortcuts import render,get_object_or_404
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
     def get_queryset(self):
-        return Question.objects.all()[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 #detail view
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 #results view
 class ResultsView(generic.DetailView):
